4220844fa41c803f2f5b45fbbe62ebfd6367616e
Drew-C-Schnabel <drew.schnabel@va.gov>
2024-01-15 11:53:28 -0500

    Update createARCHER.sql

--- Insurance-Services/ARCHER/database/createARCHER.sql
+++ Insurance-Services/ARCHER/database/createARCHER.sql
@@ -2,28 +2,28 @@ CREATE DATABASE ARCHER;
 
 USE ARCHER;
 
-CREATE TABLE ARCHER.PremiumPayments (
+CREATE TABLE ARCHER_PremiumPayments (
     ID INT IDENTITY(1,1) NOT NULL,
     PolicyNumber VARCHAR(20) NOT NULL,
-    PaymentCode INT(2) NOT NULL,
+    PaymentCode INT NOT NULL,
     SuspenseAmount DECIMAL(20,2),
     PaymentAmount DECIMAL(20,2),
-    CurrPaidToDate INT(8),
-    NewPaidToDate INT(8),
-    PaymentStatus INT(2) NOT NULL,
+    CurrPaidToDate INT,
+    NewPaidToDate INT,
+    PaymentStatus INT NOT NULL,
     PaymentDate DATETIME,
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE ARCHER.LoanPayments (
+CREATE TABLE ARCHER_LoanPayments (
     ID INT IDENTITY(1,1) NOT NULL,
     PolicyNumber VARCHAR(20) NOT NULL,
-    PaymentCode INT(2) NOT NULL,
+    PaymentCode INT NOT NULL,
     SuspenseAmount DECIMAL(20,2),
     PaymentAmount DECIMAL(20,2),
     PriorLoanBalance DECIMAL(20,2),
     NewLoanBalance DECIMAL(20,2),
-    PaymentStatus INT(2) NOT NULL,
+    PaymentStatus INT NOT NULL,
     PaymentDate DATETIME,
     PRIMARY KEY (ID)
 );
