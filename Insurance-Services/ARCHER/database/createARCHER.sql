8a83412f3477f4193784bbdbcd83f2b730a0a5f7
Drew-Schnabel (VA) <drew.schnabel@va.gov>
2024-02-06 15:58:41 -0500

    Update createARCHER.sql

--- Insurance-Services/ARCHER/database/createARCHER.sql
+++ Insurance-Services/ARCHER/database/createARCHER.sql
@@ -1,29 +1,23 @@
-CREATE DATABASE ARCHER;
+--//Note: This uses the EIN Database currently, so this script is subject to change if used long-term.
 
-USE ARCHER;
 
-CREATE TABLE ARCHER_PremiumPayments (
-    ID INT IDENTITY(1,1) NOT NULL,
-    PolicyNumber VARCHAR(20) NOT NULL,
-    PaymentCode INT NOT NULL,
-    SuspenseAmount DECIMAL(20,2),
-    PaymentAmount DECIMAL(20,2),
-    CurrPaidToDate INT,
-    NewPaidToDate INT,
-    PaymentStatus INT NOT NULL,
-    PaymentDate DATETIME,
-    PRIMARY KEY (ID)
-);
-
-CREATE TABLE ARCHER_LoanPayments (
-    ID INT IDENTITY(1,1) NOT NULL,
-    PolicyNumber VARCHAR(20) NOT NULL,
-    PaymentCode INT NOT NULL,
-    SuspenseAmount DECIMAL(20,2),
-    PaymentAmount DECIMAL(20,2),
-    PriorLoanBalance DECIMAL(20,2),
-    NewLoanBalance DECIMAL(20,2),
-    PaymentStatus INT NOT NULL,
-    PaymentDate DATETIME,
-    PRIMARY KEY (ID)
-);
+USE DevData;
+go
+ 
+ 
+CREATE TABLE Archer (
+			ID INT IDENTITY(1,1) NOT NULL
+		,	LogDate DATETIME
+		,	PolicyNumber VARCHAR(20) NOT NULL
+		,	PaymentCode INT NOT NULL
+		,	SuspenseAmount DECIMAL(20,2)
+		,	PaymentAmount DECIMAL(20,2)
+		,	Paid_to_date INT
+		,	NewPaid_to_date INT
+		,	PaymentStatus VARCHAR(10) NOT NULL
+		,	PaymentDate INT NOT NULL
+		,	PriorLoanBalance DECIMAL(20,2)
+		,	NewLoanBalance DECIMAL(20,2)
+		, 	LIPASResponse VARCHAR(MAX)
+		PRIMARY KEY (ID)
+		)
