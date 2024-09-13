0b5c6dca3397f74a9755956413629da2e0fda0eb
Drew-C-Schnabel <drew.schnabel@va.gov>
2024-09-13 07:51:03 -0400

    Commit to builder

--- server/public/resources/@department-of-veterans-affairs/formation/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.js$': '<rootDir>/packages/formation/scripts/babel-transformer.js',
-  },
-  testEnvironment: 'jsdom',
-  rootDir: '../..',
-  testMatch: [`<rootDir>/packages/formation/__tests__/**/*.test.js`],
-};
